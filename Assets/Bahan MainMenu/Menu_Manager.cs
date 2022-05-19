4fdfe6b39c6e1302aa0be8734df72d9995cf44fe
salsaskarin <karinsalsabila17@gmail.com>
2022-05-19 21:47:29 +0700

    progress 2

---
+++ Assets/Bahan MainMenu/Menu_Manager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Manager : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+    public GameObject InstruksiPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        InstruksiPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButtonClicked()
+    {
+        Application.LoadLevel("Labirin");
+    }
+    public void CreditsButtonClicked()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+        InstruksiPanel.SetActive(false);
+    }
+    public void InstruksiButtonClicked()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        InstruksiPanel.SetActive(true);
+    }
+    public void Quit_Clicked()
+    {
+        Application.Quit();
+    }
+    public void BackButtonClicked()
+    {
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        InstruksiPanel.SetActive(false);
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,47 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour
+{
+    public float speed;
+    public Text skor;
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKey (KeyCode.UpArrow)) {
+            transform.Translate (0,0,speed);
+        }
+        if (Input.GetKey (KeyCode.DownArrow)) {
+            transform.Translate (0,0,-speed);
+        }
+        if (Input.GetKey (KeyCode.RightArrow)) {
+            transform.Translate (speed,0,0);
+        }
+        if (Input.GetKey (KeyCode.LeftArrow)) {
+            transform.Translate (-speed,0,0);
+        }
+    }
+
+    void OnTriggerEnter(Collider obj){
+        if (obj.gameObject.tag == "target") {
+            Destroy (obj.gameObject);
+            score += 10;
+            skor.text = "Skor : " + score.ToString();
+            GameObject.Find ("suara target").GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void OnCollisionEnter(Collision obj){
+        if (obj.gameObject.name == "wall"){
+            GameObject.Find ("suara wall").GetComponent<AudioSource>().Play();
+        }
+    }
+}
