958cb1e32be1e2c06366256d32da508dd475833a
muhamadfachri12 <fachrigunners@gmail.com>
2022-05-31 21:44:05 +0700

    update

---
+++ Assets/Folder buat diZIP/script/Demo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine ;
+
+public class Demo : MonoBehaviour {
+
+   [SerializeField] Timer timer1 ;
+  
+
+   private void Start () {
+      timer1.SetDuration(6).Begin () ;
+
+      
+   }
+
+}
---
+++ Assets/Folder buat diZIP/script/events.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class events : MonoBehaviour
+{
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("Labirin");
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    /*
+    public void NextGame2()
+    {
+        SceneManager.LoadScene("Labirin 2");
+    }
+    */
+}
