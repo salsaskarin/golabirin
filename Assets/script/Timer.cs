dfd64bb9a7ac7455929c1de50e5dd84f716f18ec
salsaskarin <karinsalsabila17@gmail.com>
2022-06-02 12:58:29 +0700

    finished

--- Assets/script/Timer.cs
+++ Assets/script/Timer.cs
@@ -1,117 +1,38 @@
-﻿using UnityEngine ;
-using UnityEngine.UI ;
-using UnityEngine.Events ;
-using System.Collections ;
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Timer : MonoBehaviour {
-   [Header ("Timer UI references :")]
-   [SerializeField] private Image uiFillImage ;
-   [SerializeField] private Text uiText ;
+public class Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    public float startingTime;
+    public GameObject WinPanel;
+    public GameObject OverPanel;
 
-   public int Duration { get; private set; }
+    [SerializeField] public TextMeshProUGUI countdownText;
 
-   public bool IsPaused { get; private set; }
+    void Start()
+    {
+        currentTime = startingTime;
+    }
 
-   private int remainingDuration ;
-
-   // Events --
-   private UnityAction onTimerBeginAction ;
-   private UnityAction<int> onTimerChangeAction ;
-   private UnityAction onTimerEndAction ;
-   private UnityAction<bool> onTimerPauseAction ;
-
-   private void Awake () {
-      ResetTimer () ;
-   }
-
-   private void ResetTimer () {
-      uiText.text = "00:00" ;
-      uiFillImage.fillAmount = 0f ;
-
-      Duration = remainingDuration = 0 ;
-
-      onTimerBeginAction = null ;
-      onTimerChangeAction = null ;
-      onTimerEndAction = null ;
-      onTimerPauseAction = null ;
-
-      IsPaused = false ;
-   }
-
-   public void SetPaused (bool paused) {
-      IsPaused = paused ;
-
-      if (onTimerPauseAction != null)
-         onTimerPauseAction.Invoke (IsPaused) ;
-   }
-
-
-   public Timer SetDuration (int seconds) {
-      Duration = remainingDuration = seconds ;
-      return this ;
-   }
-
-   //-- Events ----------------------------------
-   public Timer OnBegin (UnityAction action) {
-      onTimerBeginAction = action ;
-      return this ;
-   }
-
-   public Timer OnChange (UnityAction<int> action) {
-      onTimerChangeAction = action ;
-      return this ;
-   }
-
-   public Timer OnEnd (UnityAction action) {
-      onTimerEndAction = action ;
-      return this ;
-   }
-
-   public Timer OnPause (UnityAction<bool> action) {
-      onTimerPauseAction = action ;
-      return this ;
-   }
-
-
-
-
-
-   public void Begin () {
-      if (onTimerBeginAction != null)
-         onTimerBeginAction.Invoke () ;
-
-      StopAllCoroutines () ;
-      StartCoroutine (UpdateTimer ()) ;
-   }
-
-   private IEnumerator UpdateTimer () {
-      while (remainingDuration > 0) {
-         if (!IsPaused) {
-            if (onTimerChangeAction != null)
-               onTimerChangeAction.Invoke (remainingDuration) ;
-
-            UpdateUI (remainingDuration) ;
-            remainingDuration-- ;
+    void Update()
+    {
+        
+         if (WinPanel.activeSelf)
+         {
+            currentTime = currentTime;
+         }else{
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString("0");
          }
-         yield return new WaitForSeconds (1f) ;
-      }
-      End () ;
-   }
-
-   private void UpdateUI (int seconds) {
-      uiText.text = string.Format ("{0:D2}:{1:D2}", seconds / 60, seconds % 60) ;
-      uiFillImage.fillAmount = Mathf.InverseLerp (0, Duration, seconds) ;
-   }
-
-   public void End () {
-      if (onTimerEndAction != null)
-         onTimerEndAction.Invoke () ;
-
-      ResetTimer () ;
-   }
-
-
-   private void OnDestroy () {
-      StopAllCoroutines () ;
-   }
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            OverPanel.SetActive(true);
+        }
+    }
 }
